b584b610a31bd6c3ae8cffb40b59de6fde92bcb9
Olakunle Olaniyi <olakunle.olaniyi@billpay.de>
2016-05-22 16:24:27 +0200

    corrected git url

--- setup.py
+++ setup.py
@@ -9,3 +9,32 @@
 # apt packages 
 # libpng-dev , libfreetype6-dev
 
+from setuptools import setup
+
+def readme():
+    with open('README.txt') as f:
+        return f.read()
+
+setup(name='pg_pybench',
+      version='1.0',
+      description='PostgreSQL monitoring checks',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: System :: Benchmark',
+      ],
+      url='https://github.com/rugging24/pg_pybench',
+      author='Olakunle Olaniyi',
+      author_email='rugging24@gmail.com',
+      license='PostgreSQL License',
+      packages=['pg_pybench'],
+      install_requires=[
+          'psycopg2',
+	  'psutil',
+	  'pyudev',
+      ],
+      scripts=['bin/pg_pybench'],
+      include_package_data=True,
+      zip_safe=False)
