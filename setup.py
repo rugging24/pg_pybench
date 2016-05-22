bca48d6f69c64fb7678eb0d70077b46d7280e1c3
Olakunle Olaniyi <olakunle.olaniyi@billpay.de>
2016-05-22 17:50:17 +0200

    eds

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def readme():
         return f.read()
 
 setup(name='pg_pybench',
-      version='1.1',
+      version='1.6',
       description='PostgreSQL benchmarking',
       long_description=readme(),
       classifiers=[
